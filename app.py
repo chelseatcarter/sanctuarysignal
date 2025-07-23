04495c445c28f6b86373ecc4145ebf5ce05bd2f9
Osunmo <oluwadamilaresunmola1@gmail.com>
2025-07-23 16:38:01 -0400

    Added flask code for sign up and login to get users information. Also added debugging code which queries the backend for the user details

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User
 import random
+import json
 
 app = Flask(__name__)
 
@@ -18,18 +19,56 @@ def signup():
     if request.method == 'POST':
         data = request.get_json()
         app.logger.info(f"Signup data: {data}")
-        return jsonify({"message": "Signup received"})
+
+        if User.query.filter_by(username=data['username']).first():
+            return jsonify({"error": "Username already exists"}), 400
+
+        user = User(
+            username=data['username'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            phone_number=data['phone_number'],
+            zip_code=data['zip_code'],
+        )
+        user.set_password(data['password'])
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"message": "Signup successful"}), 201
 
     return render_template('signup.html')
+@app.route('/debug/users')
+def list_users():
+    users = User.query.all()
+    return jsonify([
+        {
+            "id": user.id,
+            "username": user.username,
+            "first_name": user.first_name,
+            "phone": user.phone_number
+        } for user in users
+    ])
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         data = request.get_json()
         app.logger.info(f"Login data: {data}")
-        return jsonify({"message": "Login received"})
+
+        user = User.query.filter_by(username=data['username']).first()
+        if user and user.check_password(data['password']):
+            return jsonify({
+                "message": "Login successful",
+                "user_id": user.id,
+                "verified": user.verified,
+                "banned": user.banned
+            }), 200
+
+        return jsonify({"error": "Invalid credentials"}), 401
 
     return render_template('login.html')
+
 
 @app.route('/report', methods=['GET', 'POST'])
 def report():
