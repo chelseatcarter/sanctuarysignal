c4dc92a4e528f903c4fe736e478344d77fe3fd0b
chelseatcarter <chelseatcarter@gmail.com>
2025-07-25 17:23:27 -0400

    Added alerts and alert votes tables

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 db = SQLAlchemy()
 
@@ -32,3 +33,25 @@ class ZipCode(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     zip_code = db.Column(db.String(10), primary_key=True)
     county_name = db.Column(db.String(100), nullable=False)
+
+class Alert(db.Model):
+    __tablename__ = 'alerts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    photo = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    address = db.Column(db.String(255), nullable=False)
+    alert_type = db.Column(db.String(100), nullable=False)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+    false_votes = db.Column(db.Integer, default=0)
+
+    user = db.relationship('User', backref='alerts')
+
+class AlertVote(db.Model):
+    __tablename__ = 'alert_votes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    alert_id = db.Column(db.Integer, db.ForeignKey('alerts.id'), nullable=False)
+    is_false = db.Column(db.Boolean, nullable=False)
+    __table_args__ = (db.UniqueConstraint('user_id', 'alert_id', name='_user_alert_uc'),)
