1ecf8a4b8e7e8c3fd5e22ae88d2a8150eb86bad7
chelseatcarter <chelseatcarter@gmail.com>
2025-07-27 10:25:57 -0400

    Added lat, lng so that the map will center to the user's lat and lng based on zip code

--- models.py
+++ models.py
@@ -32,6 +32,9 @@ class ZipCode(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     zip_code = db.Column(db.String(10), primary_key=True)
     county_name = db.Column(db.String(100), nullable=False)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
 
 class Alert(db.Model):
     __tablename__ = 'alerts'
