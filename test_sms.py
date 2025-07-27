4fa08b7266219ec389f50f09319c8a357c47a4ef
Osunmo <oluwadamilaresunmola1@gmail.com>
2025-07-27 15:38:56 -0400

    Removed credentials

--- test_sms.py
+++ test_sms.py
@@ -1,6 +1,9 @@
 from twilio.rest import Client
-account_sid = 'ACc8131e14d6d03ece7885f9a9778411e6'
-auth_token = 'a68dffdf8f3c99e7960fbbea06fdafa9'
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 message = client.messages.create(
   messaging_service_sid='MG4c367cc6b146a8b8215fb3a389f4108d',
